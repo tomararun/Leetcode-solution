a5832fe3d1bc6cbad7af213c53e6607de6223df3
Arun Kumar Tomar <95977898+tomararun@users.noreply.github.com>
2023-12-14 22:10:00 +0530

    Time: 3 ms (71.56%), Space: 42.9 MB (91.83%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,23 @@
+
+public class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        if (nums.length < 3) {
+            return nums[nums.length - 1];
+        }
+
+        int count = 0;
+
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] != nums[i - 1]) {
+                count++;
+            }
+
+            if (count == 2) {
+                return nums[i - 1];
+            }
+        }
+
+        return nums[nums.length - 1];
+    }
+}
