f5feb4dab87bc20a070712b25f59a35de230051c
Arun Kumar Tomar <95977898+tomararun@users.noreply.github.com>
2023-12-15 23:42:00 +0530

    Time: 27 ms (46.65%), Space: 42 MB (46.60%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,34 @@
+
+
+class Solution {
+    public void dfs(Node node , Node copy , Node[] visited){
+        visited[copy.val] = copy;// store the current node at it's val index which will tell us that this node is now visited
+        
+//         now traverse for the adjacent nodes of root node
+        for(Node n : node.neighbors){
+//             check whether that node is visited or not
+//              if it is not visited, there must be null
+            if(visited[n.val] == null){
+//                 so now if it not visited, create a new node
+                Node newNode = new Node(n.val);
+//                 add this node as the neighbor of the prev copied node
+                copy.neighbors.add(newNode);
+//                 make dfs call for this unvisited node to discover whether it's adjacent nodes are explored or not
+                dfs(n , newNode , visited);
+            }else{
+//                 if that node is already visited, retrieve that node from visited array and add it as the adjacent node of prev copied node
+//                 THIS IS THE POINT WHY WE USED NODE[] INSTEAD OF BOOLEAN[] ARRAY
+                copy.neighbors.add(visited[n.val]);
+            }
+        }
+        
+    }
+    public Node cloneGraph(Node node) {
+        if(node == null) return null; // if the actual node is empty there is nothing to copy, so return null
+        Node copy = new Node(node.val); // create a new node , with same value as the root node(given node)
+        Node[] visited = new Node[101]; 
+        Arrays.fill(visited , null); 
+        dfs(node , copy , visited); 
+        return copy; 
+    }
+}
