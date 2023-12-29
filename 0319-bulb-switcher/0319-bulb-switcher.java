00a6e33521bfb034faea9d82be21de099d8d4bc5
Arun Kumar Tomar <95977898+tomararun@users.noreply.github.com>
2023-12-30 02:04:36 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (19.29%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -0,0 +1,12 @@
+class Solution {
+
+    // the numbers which have odd number of facters will remains on
+    // and the numbers which have even number of facters will remains off
+
+    // which numbers have odd number of factors
+    // only perfect square number will have odd number of factors
+    public int bulbSwitch(int n) {
+        
+        return (int)Math.sqrt(n);
+    }
+}
