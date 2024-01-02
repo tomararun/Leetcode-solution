b2edb41021dd122867e7906868570b690128eae0
Arun Kumar Tomar <95977898+tomararun@users.noreply.github.com>
2024-01-03 01:58:38 +0530

    Time: 2 ms (39.74%), Space: 55.3 MB (85.45%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,35 @@
+public class Solution {
+public int firstMissingPositive(int[] nums) {
+    int n = nums.length;
+    
+    // 1. mark numbers (num < 0) and (num > n) with a special marker number (n+1) 
+    
+    for (int i = 0; i < n; i++) {
+        if (nums[i] <= 0 || nums[i] > n) {
+            nums[i] = n + 1;
+        }
+    }
+    
+    // 2. mark each cell appearing in the array, by converting the index for that number to negative
+    for (int i = 0; i < n; i++) {
+        int num = Math.abs(nums[i]);
+        if (num > n) {
+            continue;
+        }
+        num--; 
+        if (nums[num] > 0) { // prevents double negative operations
+            nums[num] = -1 * nums[num];
+        }
+    }
+    
+    // 3. find the first cell which isn't negative (doesn't appear in the array)
+    for (int i = 0; i < n; i++) {
+        if (nums[i] >= 0) {
+            return i + 1;
+        }
+    }
+    
+    // 4. no positive numbers were found, which means the array contains all numbers 1..n
+    return n + 1;
+}
+}
