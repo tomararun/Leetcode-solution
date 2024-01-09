5ac4583abd1528f465c7204508b95d928aa81d98
Arun Kumar Tomar <95977898+tomararun@users.noreply.github.com>
2024-01-09 11:19:42 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (18.19%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length() <= 1) return true;
+        int a = word.charAt(0) - 'A';
+        // if 1st char is lowercase, all chars should be lowercase
+        if(a >= 32){
+            for(int i=1; i<word.length(); i++){
+                if(word.charAt(i) - 'A' < 32) return false;
+            }
+        }
+        
+        else{
+            // if 2nd char is uppercase, all char should be uppercase
+            if(word.charAt(1) - 'A' < 32){
+                for(int i=2; i<word.length(); i++){
+                    if(word.charAt(i) - 'A' >= 32) return false;
+                }
+            }
+            
+            else{
+                for(int i=1; i<word.length(); i++){
+                    if(word.charAt(i) - 'A' < 32) return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
