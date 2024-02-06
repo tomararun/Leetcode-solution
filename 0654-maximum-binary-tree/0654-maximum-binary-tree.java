9513c31f76b8e163ae595ba3f58d28ffac6733a1
Arun Kumar Tomar <95977898+tomararun@users.noreply.github.com>
2024-02-06 11:33:10 +0530

    Time: 1 ms (100.00%), Space: 44.7 MB (67.82%) - LeetHub

---
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -0,0 +1,27 @@
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return construct(0,nums.length-1,nums);
+
+    }
+
+    public TreeNode construct(int start,int end,int[]nums){
+        if(start>end){
+            return null;
+        }
+        int root=0;
+        int rootInd=0;
+        
+        for(int i=start;i<=end;i++){
+            if(nums[i]>=root){
+                root=nums[i];
+                rootInd=i;
+            }
+        }
+
+        TreeNode head=new TreeNode(root);
+        head.left=construct(start,rootInd-1,nums);
+        head.right=construct(rootInd+1,end,nums);
+        return head;
+    }
+
+}
